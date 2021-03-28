f7c3b1c87960351c40cd2a34e9af1f265b87f9b6
nickxgrom <52834219+nickxgrom@users.noreply.github.com>
2021-03-28 21:53:21 +0600

    Add List.vue initial markup

--- src/app.vue
+++ src/app.vue
@@ -1,17 +1,34 @@
 <template>
-    <div>
+    <div class="wrapper">
         <h1>ALSECO test task</h1>
+        <list
+            :rows="8"
+            :columns="4"
+        ></list>
     </div>
 </template>
 
 <script>
-export default {
-name: "app"
-}
+import List from "./components/List.vue";
+    export default {
+        name: "app",
+        components: {
+            List,
+        }
+    }
 </script>
 
 <style>
+    @import url('https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;700&display=swap');
+
     body {
         margin: 0;
+        font-family: 'Ubuntu', sans-serif;
     }
+
+    .wrapper {
+        width: 80%;
+        margin: 0 auto;
+    }
+
 </style>
---
+++ src/components/List.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="employee-list">
+        <table class="table_block">
+            <tr class="table-header">
+                <th
+                    v-for="c in columns"
+                >{{c}} column</th>
+            </tr>
+            <tr
+                v-for="i in rows"
+                :class="{ 'row-diff-color': !(i%2) }"
+            >
+                <td
+                    v-for="j in columns"
+                >
+                    {{i}} | {{j}}
+                </td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+    name: "List",
+        props: {
+            columns: Number,
+            rows: Number
+        }
+    }
+</script>
+
+<style scoped>
+    .employee-list {
+        border-radius: 10px 10px 0 0;
+        overflow: hidden;
+    }
+
+    table {
+        font-size: 1.2em;
+        text-align: center;
+        width: 100%;
+        border-collapse: collapse;
+        user-select: none;
+        border-bottom: 2px solid #01987A;
+    }
+
+    .table-header {
+        background-color: #01987A;
+        border-radius: 7px;
+        border-bottom: none;
+    }
+
+    td {
+        padding: 7px 0;
+    }
+
+    tr {
+        border-bottom: 1px solid #E0E0E0;
+    }
+
+    th {
+        padding: 10px 0;
+        color: #FFF;
+    }
+
+    .row-diff-color {
+        background: #F3F3F3;
+    }
+</style>
