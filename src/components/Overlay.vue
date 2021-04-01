6a1e4ca5623c429409723b5dff20f34aa90d5734
nickxgrom <52834219+nickxgrom@users.noreply.github.com>
2021-04-01 13:37:16 +0600

    Add Overlay.vue component

---
+++ src/components/Overlay.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="overlay">
+        <slot class="slot"></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Overlay",
+}
+</script>
+
+<style scoped>
+    .overlay {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, .6);
+    }
+
+    .slot {
+        margin: 0 auto;
+    }
+</style>
